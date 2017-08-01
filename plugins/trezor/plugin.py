fe4aadfe828205550e1ed4a200cfe565994363b2
Saleem Rashid <dev@saleemrashid.com>
2017-08-01 16:02:10 +0100

    trezor: Use Bitcoin Gold as coin_name

--- plugins/trezor/plugin.py
+++ plugins/trezor/plugin.py
@@ -137,7 +137,7 @@ class TrezorCompatiblePlugin(HW_PluginBase):
         return client
 
     def get_coin_name(self):
-        return "Testnet" if NetworkConstants.TESTNET else "Bitcoin"
+        return None if NetworkConstants.TESTNET else "Bitcoin Gold"
 
     def initialize_device(self, device_id, wizard, handler):
         # Initialization method
