caa75b0244e7ddecdb3ceaa4c389a0c4a42dc182
wilsonmeier <wilson.meier@gmail.com>
2018-03-26 22:11:24 +0200

    Changed text interaction flags to allow text copy and open url This fixes #23

--- gui/qt/main_window.py
+++ gui/qt/main_window.py
@@ -554,7 +554,7 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, PrintError):
     def show_report_bug(self):
         msg = ' '.join([
             _("Please report any bugs as issues on github:<br/>"),
-            "<a href=\"https://github.com/btcgpu/electrum/issues\">https://github.com/btcgpu/electrum/issues</a><br/><br/>",
+            "<a href=\"https://github.com/BTCGPU/electrum/issues\">https://github.com/BTCGPU/electrum/issues</a><br/><br/>",
             _("Before reporting a bug, upgrade to the most recent version of ElectrumG (latest release or git HEAD), and include the version number in your report."),
             _("Try to explain not only what the bug is, but how it occurs.")
          ])
